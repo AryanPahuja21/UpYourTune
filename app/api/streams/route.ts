e0d6733d0fc82c52f263c993bd932ee40446a009
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-09-19 16:39:37 +0530

    active stream route

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -97,23 +97,33 @@ export async function GET(req: NextRequest) {
       }
     );
   }
-  const streams = await prismaClient.stream.findMany({
-    where: {
-      userId: creatorId,
-    },
-    include: {
-      _count: {
-        select: {
-          upvotes: true,
+  const [streams, activeStream] = await Promise.all([
+    await prismaClient.stream.findMany({
+      where: {
+        userId: creatorId,
+      },
+      include: {
+        _count: {
+          select: {
+            upvotes: true,
+          },
+        },
+        upvotes: {
+          where: {
+            userId: user.id,
+          },
         },
       },
-      upvotes: {
-        where: {
-          userId: user.id,
-        },
+    }),
+    await prismaClient.currentStream.findFirst({
+      where: {
+        userId: creatorId,
       },
-    },
-  });
+      include: {
+        stream: true,
+      },
+    }),
+  ]);
 
   return NextResponse.json({
     streams: streams.map(({ _count, ...rest }) => ({
@@ -121,5 +131,6 @@ export async function GET(req: NextRequest) {
       upvotes: _count.upvotes,
       haveUpvoted: rest.upvotes.length ? true : false,
     })),
+    activeStream,
   });
 }
